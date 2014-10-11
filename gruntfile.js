caa3875489a59b1af9d37c3b33959fc2a4561c83
Richard Poole <poolieweb@yahoo.co.uk>
2014-10-11 22:22:28 +0100

    Added travis ci config

--- gruntfile.js
+++ gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     
-    grunt.registerTask('default', ['less']);
+    grunt.registerTask('default', ['less','watch']);
     // Travis CI task.
     grunt.registerTask('travis',  ['less']);
 };
