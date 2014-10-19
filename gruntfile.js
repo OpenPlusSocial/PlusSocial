6b9ff515f0b73cab68ef764ee69e2763d0349ceb
Richard Poole <poolieweb@yahoo.co.uk>
2014-10-19 21:30:15 +0100

    Added Jasmine with phamtom browser testing

--- gruntfile.js
+++ gruntfile.js
@@ -1,52 +1,71 @@
 module.exports = function (grunt) {
     grunt.initConfig({
 
-   jshint: {
+     jshint: {
         all: [
-            "Gruntfile.js",
-            "src/**/*.js",
-            "test/**/*.js"
+        "Gruntfile.js",
+        "src/**/*.js",
+        "test/**/*.js"
         ],
-       options: {
-          jshintrc: '.jshintrc'
-        },
-      },
-
-    validation: {
         options: {
-            reset: grunt.option('reset') || false,
-            stoponerror: false, 
-            path:'logs/html/html-status.json',
-            reportpath:'logs/html/html-report.json'
+          jshintrc: '.jshintrc'
+      },
+  },
+
+  validation: {
+    options: {
+        reset: grunt.option('reset') || false,
+        stoponerror: false, 
+        path:'logs/html/html-status.json',
+        reportpath:'logs/html/html-report.json'
+    },
+    files: {
+        src: ['src/ProjectIntroductionSite/*.html']
+    }
+},
+
+less: {
+    ProjectIntroductionSite: {
+        options: {
+            paths: ["assets/css"]
         },
-        files: {
-            src: ['src/ProjectIntroductionSite/*.html']
-        }
-    },
+        files: { "src/ProjectIntroductionSite/css/site.css": "src/ProjectIntroductionSite/css/less/site.less" }
+    }
+},
 
-    less: {
-        ProjectIntroductionSite: {
-            options: {
-                paths: ["assets/css"]
-            },
-            files: { "src/ProjectIntroductionSite/css/site.css": "src/ProjectIntroductionSite/css/less/site.less" }
-        }
-        
-    },
+jasmine: {
+    src: ['src/**/*.js'],
+    options: {
+        outfile: 'logs/javascript/jasmine.html',
+        host: 'http://127.0.0.1:8000/',
+        specs: 'test/**/*spec.js'
+    } 
+},  
 
-	watch: {
-		files: ["src/ProjectIntroductionSite/css/less/*.less",'src/ProjectIntroductionSite/*.html'],
-		tasks: ['less','validation']
-	}
-    });
+connect: {
+    test: {
+        port: 8000
+    }
+},
+
+watch: {
+  files: ['gruntfile.js','src/**/*.*',,'test/**/*.*'],
+  tasks: ['less','validation','jshint','jasmine']
+}
+});
 
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-html-validation');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-html-validation');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-    grunt.registerTask('default', ['less','validation','jshint']);
+
+ // Unit Testing Task
+grunt.registerTask('default', ['jshint','connect', 'jasmine','validation','less']);
+
     // Travis CI task.
-    grunt.registerTask('travis',  ['less','validation','jshint']);
+grunt.registerTask('travis',  [,'jshint','connect', 'jasmine','validation','less']);
 };
