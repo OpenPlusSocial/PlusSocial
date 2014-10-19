93a37228e9ca75f496f58e68d072497707dc1e4f
Richard Poole <poolieweb@yahoo.co.uk>
2014-10-19 15:19:57 +0100

    Added JsHint and option file to grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,40 +1,52 @@
 module.exports = function (grunt) {
     grunt.initConfig({
 
-
-
-        validation: {
-    options: {
-        reset: grunt.option('reset') || false,
-        stoponerror: false, 
-        path:'logs/html/html-status.json',
-        reportpath:'logs/html/html-report.json'
-    },
-    files: {
-        src: ['src/ProjectIntroductionSite/*.html']
-    }
-},
-
-        less: {
-            ProjectIntroductionSite: {
-                options: {
-                    paths: ["assets/css"]
-                },
-                files: { "src/ProjectIntroductionSite/css/site.css": "src/ProjectIntroductionSite/css/less/site.less" }
-            }
-            
+   jshint: {
+        all: [
+            "Gruntfile.js",
+            "src/**/*.js",
+            "test/**/*.js"
+        ],
+       options: {
+          jshintrc: '.jshintrc'
         },
+      },
+
+    validation: {
+        options: {
+            reset: grunt.option('reset') || false,
+            stoponerror: false, 
+            path:'logs/html/html-status.json',
+            reportpath:'logs/html/html-report.json'
+        },
+        files: {
+            src: ['src/ProjectIntroductionSite/*.html']
+        }
+    },
+
+    less: {
+        ProjectIntroductionSite: {
+            options: {
+                paths: ["assets/css"]
+            },
+            files: { "src/ProjectIntroductionSite/css/site.css": "src/ProjectIntroductionSite/css/less/site.less" }
+        }
+        
+    },
 
 	watch: {
 		files: ["src/ProjectIntroductionSite/css/less/*.less",'src/ProjectIntroductionSite/*.html'],
 		tasks: ['less','validation']
 	}
     });
+
+
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-html-validation');
-    
-    grunt.registerTask('default', ['less','validation']);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['less','validation','jshint']);
     // Travis CI task.
-    grunt.registerTask('travis',  ['less','validation']);
+    grunt.registerTask('travis',  ['less','validation','jshint']);
 };
