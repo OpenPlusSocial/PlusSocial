3ff6eca8dd737740d2d4aea703a13ffe72ec0c4d
Richard Poole <poolieweb@yahoo.co.uk>
2014-10-21 22:27:17 +0100

    Cleaned up formating

--- test/FeedTestSiteSpecs/feedTestpage-spec.js
+++ test/FeedTestSiteSpecs/feedTestpage-spec.js
@@ -4,24 +4,24 @@ jasmine.getFixtures().fixturesPath = '../../src/FeedTestSite';
 describe('Test Page Layout', function() {
 
   beforeEach(function() {
-     loadFixtures('index.html');  
+    loadFixtures('index.html');
   });
 
   afterEach(function() {
 
-  });    
+  });
 
   describe('Given the test page', function() {
 
     it('should has a header', function() {
 
-    var header = $('h1');
- 
-    expect(header).toHaveText('Feed Tests');
-      
+      var header = $('h1');
+
+      expect(header).toHaveText('Feed Tests');
+
     });
 
-       
+
   });
 
 });
